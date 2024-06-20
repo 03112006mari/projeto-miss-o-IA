59e56503fa1cc77cf68c8b07d243f3006261b843
03112006mari <118907487+03112006mari@users.noreply.github.com>
2024-06-20 08:35:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: Qual país vc acha a melhor cúlinaria?",
+        enunciado: "Qual país vc acha a melhor cúlinaria?",
         alternativas: [
             {
                 texto: "Ítalia",
